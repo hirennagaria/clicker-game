a144d70ba5dc5d703618eae2f90de7f8f03b6391
Hiren Nagaria <hirennagaria@yahoo.co.in>
2020-06-11 01:04:22 +0530

    Third level completed

--- script.js
+++ script.js
@@ -1,81 +1,144 @@
 let money = 0;
-let invest = 0;
+let stocks = 0;
+let bonds = 0;
+let property = 0;
 
 function make() {
   money++;
-  renderMoney(money);
+  renderMoney();
 }
 
-function buy() {
+function buyStocks() {
   if (money >= 5) {
     money -= 5;
-    invest++;
-    renderInvest(invest);
+    stocks++;
+    renderStocks();
   }
 }
 
-/*
-function render() {
-  money_output.innerHTML = money;
-  invest_output.innerHTML = invest;
+function buyBonds() {
+  if (money >= 100 && stocks >= 5) {
+    money -= 100;
+    stocks -= 5;
+    bonds++;
+    renderBonds();
+  }
 }
-*/
+
+function buyPropperty() {
+  if (money >= 5000 && stocks >= 200 && bonds >= 50) {
+    money -= 5000;
+    stocks -= 200;
+    bonds -= 50;
+    property++;
+    renderProperty();
+  }
+}
+
+
+function renderMoney() {
+  money_output.innerHTML = money;
+  //stocks_output.innerHTML = stocks;
+}
+
+function renderStocks() {
+  //money_output.innerHTML = money;
+  stocks_output.innerHTML = stocks;
+}
+
+function renderBonds() {
+  //money_output.innerHTML = money;
+  bonds_output.innerHTML = bonds;
+}
+
+function renderProperty() {
+  //money_output.innerHTML = money;
+  property_output.innerHTML = property;
+}
+
+
 
 function update() {
-  if (invest) {
-    money += invest;
+  if (stocks) {
+    money += stocks;
+  }
+  if(bonds) {
+    money += bonds * 3;
+  }
+  if(property) {
+    money += property * 20;
   }
 }
 
+// Where el is the DOM element you'd like to test for visibility
+function isHidden(element) {
+  var el = document.getElementById(element);
+    return (el.offsetParent === null)
+}
 
+function checkNotifications() {
+  if (money >= 100 && stocks >= 5 && isHidden("buyBonds")) {
+    document.getElementById("buyBonds").style.display = 'block';
+    document.getElementById("bondsProgress").style.display = 'none';
+    document.getElementById("propertyProgress").style.display = 'block';
+    document.getElementById("bondsNotification").style.display = 'block';
+  }
+
+  if (money >= 5000 && stocks >= 200 && bonds >= 50 && isHidden("buyProperty")) {
+    document.getElementById("buyProperty").style.display = 'block';
+    document.getElementById("propertyProgress").style.display = 'none';
+    document.getElementById("propertyNotification").style.display = 'block';
+  }
+}
+
+function renderProgress() {
+  if(!isHidden('bondsProgress')){
+    let progressValue = 0;
+    let moneyValue = money/2;
+    moneyValue = (moneyValue>50) ? 50 : moneyValue;
+
+   let stockValue = stocks * 10;
+   stockValue = (stockValue>50) ? 50 : stockValue;
+   progressValue = moneyValue + stockValue;
+    bondsProgress.value = progressValue;
+  }
+  if(!isHidden('propertyProgress')){
+    let progressValue = 0;
+    let moneyValue = money/150.01;
+    moneyValue = (moneyValue>33.33) ? 33.33 : moneyValue;
+
+   let stockValue = stocks / 6;
+   stockValue = (stockValue>33.33) ? 33.33 : stockValue;
+
+   let bondValue = bonds / 1.5;
+   bondValue = (bondValue>33.34) ? 33.34 : bondValue;
+   progressValue = moneyValue + stockValue + bondValue;
+    propertyProgress.value = progressValue;
+  }
+}
 
 // loop
 setInterval(() => {
   update()
-  renderMoney(money);
-  renderInvest(invest);
-
+  renderMoney();
+  renderStocks();
+  renderBonds();
+  renderProperty();
+  checkNotifications();
+  renderProgress();
 }, 500) // 500ms = twice a second
 
 
-//var logEl = document.querySelector('.battery-log');
+document.addEventListener('DOMContentLoaded', () => {
 
-var totalWorth = {
-  money: 0,
-  invest: 0
-}
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
 
-
-
-var renderMoney = function(moneyTarget){
-
-anime({
-  targets: totalWorth,
-  money: moneyTarget,
-  //invest: investTarget,
-  round: 1,
-  easing: 'linear',
-  update: function() {
-    money_output.innerHTML = totalWorth.money;
-    //invest_output.innerHTML = totalWorth.invest;
-  }
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
 });
-}
-
-var renderInvest = function(investTarget){
-
-anime({
-  targets: totalWorth,
-  //money: moneyTarget,
-  invest: investTarget,
-  round: 1,
-  easing: 'linear',
-  update: function() {
-    //money_output.innerHTML = totalWorth.money;
-    invest_output.innerHTML = totalWorth.invest;
-  }
-});
-}
 
 
-//render(0, 0);
+
